d9e873391d8a697a96a7db570f25d3bc04242e80
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2021-11-13 16:50:12 -0500

    adds Visit model and association

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,6 @@ class Link < ApplicationRecord
   validates :external_url, url: { allow_blank: false }
   validates_presence_of :token
   validates_length_of :token, is: 8
+
+  has_many :visits, counter_cache: true
 end
