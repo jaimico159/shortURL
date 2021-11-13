f2989bee72f190da11e5acd886b4208b6bc64a5e
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2021-11-13 17:58:19 -0500

    adds routes to match actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :links, only: %i[new show create]
+  resources :links, only: %i[index new create]
+  get '/:token', to: 'links#token_redirection', as: :token_redirection
+  get '/:token/info', to: 'links#show', as: :link_info
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'links#new'
 end
