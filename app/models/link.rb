28949572f0119f623265c64a93f819d5ea78f2e4
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2021-11-13 16:48:20 -0500

    adds first validation rules to links

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,5 @@
 class Link < ApplicationRecord
+  validates :external_url, url: { allow_blank: false }
+  validates_presence_of :token
+  validates_length_of :token, is: 8
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_url_of(:external_url) }
+  it { is_expected.to validate_presence_of(:token) }
+  it { is_expected.to validate_length_of(:token).is_equal_to(8) }
 end
