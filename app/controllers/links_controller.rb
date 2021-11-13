fa78befbd7f8a1aa30d2c2fec60f95801ffbf745
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2021-11-13 17:33:34 -0500

    adds logic to create links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -7,7 +7,18 @@ class LinksController < ApplicationController
     @link = Link.new
   end
 
-  def create; end
+  def create
+    link = Link.new link_params
+    resp = TokenGenerator.call
+
+    link.token = resp[:data][:token]
+
+    if link.save
+      redirect_to link_path(link), notice: 'Link generated successfully!'
+    else
+      redirect_to root_path, alert: link.errors.full_messages.join(',')
+    end
+  end
 
   private
 
