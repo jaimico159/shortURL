4cbe7e4de72a3eeb8e97de064f3b456ca2640650
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2021-11-13 17:15:09 -0500

    strong params

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,7 +3,15 @@ class LinksController < ApplicationController
 
   def show; end
 
-  def new; end
+  def new
+    @link = Link.new
+  end
 
   def create; end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:external_url)
+  end
 end
